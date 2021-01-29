931ff13c209dc0101faf3c68076347e5d0e7f5b1
Lee Eun Deok <pleed0215@gmail.com>
2021-01-29 16:35:30 +0900

    some.. missing..

--- src/List/ListScroll.tsx
+++ src/List/ListScroll.tsx
@@ -1,6 +1,7 @@
 import { createStyles, List, Theme } from "@material-ui/core";
+
 import { makeStyles } from "@material-ui/styles";
-import React from "react";
+import React, { useState } from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
   createStyles({
@@ -13,12 +14,16 @@ const useStyles = makeStyles((theme: Theme) =>
   })
 );
 
-function* genItems() {
+function* genItems():Generator<string> {
   for (let i = 1; i < 1000; i++) {
     yield `Item ${i}`;
   }
 }
 
 export const ScrollingList = () => {
-  return <List></List>;
+    const classes = useStyles();
+    const [items] = useState<Array<string>>([...genItems()])
+  return <List>
+
+  </List>;
 };
